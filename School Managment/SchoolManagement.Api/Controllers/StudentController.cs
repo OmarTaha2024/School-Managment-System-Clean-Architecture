0ca61a30f48544026c09161d761926be58a4c82c
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-04-21 18:08:40 +0200

    Handle End Response With Custom Response

--- School Managment/SchoolManagement.Api/Controllers/StudentController.cs
+++ School Managment/SchoolManagement.Api/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.Api.Base;
 using SchoolManagment.Core.Features.Students.Commands.Models;
 using SchoolManagment.Core.Features.Students.Queries.Models;
 using SchoolManagment.Data.AppMetaData;
@@ -8,37 +9,28 @@ using SchoolManagment.Data.AppMetaData;
 namespace SchoolManagement.Api.Controllers
 {
     [ApiController]
-    public class StudentController : ControllerBase
+    public class StudentController : AppControllerBase
     {
-        #region Fileds
-        private readonly IMediator _mediator;
-        #endregion
-        #region CTOR
-        public StudentController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-        #endregion
         #region API
         [HttpGet(Router.StudentRouting.List)]
         public async Task<IActionResult> GetStudentsList()
         {
-            var responce = await _mediator.Send(new GetStudentListQuery());
+            var responce = await Mediator.Send(new GetStudentListQuery());
             return Ok(responce);
         }
         [HttpGet(Router.StudentRouting.GetByID)]
 
         public async Task<IActionResult> GetStudentById([FromRoute] int id)
         {
-            var responce = await _mediator.Send(new GetStudentByIDQuery(id));
-            return Ok(responce);
+            var responce = await Mediator.Send(new GetStudentByIDQuery(id));
+            return NewResult(responce);
         }
         [HttpGet(Router.StudentRouting.Create)]
 
         public async Task<IActionResult> AddStudent([FromBody] AddStudentCommand _command) 
         {
-            var responce = await _mediator.Send(_command);
-            return Ok(responce);
+            var responce = await Mediator.Send(_command);
+            return NewResult(responce);
         }
         #endregion
 
