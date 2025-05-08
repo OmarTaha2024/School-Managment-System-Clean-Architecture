39c4b7bc880d9ac704fe7b9af045786ef00f18bc
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-05-08 14:04:40 +0300

    Last Edition

--- School Managment/SchoolManagment.Service/obj/Debug/net8.0/SchoolManagment.Service.AssemblyInfo.cs
+++ School Managment/SchoolManagment.Service/obj/Debug/net8.0/SchoolManagment.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagment.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d568a5044607bc6076bae331cb624a06f58abd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagment.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagment.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
