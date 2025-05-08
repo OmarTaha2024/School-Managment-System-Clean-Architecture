0e9e1698fac0ac298bdb291681e7a4d5dce4cb0c
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-05-08 13:43:44 +0300

    last changes

--- School Managment/SchoolManagment.Service/obj/Debug/net8.0/SchoolManagment.Service.AssemblyInfo.cs
+++ School Managment/SchoolManagment.Service/obj/Debug/net8.0/SchoolManagment.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagment.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b70ce9229062d9e55815c1bb02241d2b4e653e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagment.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagment.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
