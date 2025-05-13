3bec111f35230456db17afff948cd11ea6dabf33
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-05-13 15:25:46 +0300

    Add new files and changes

--- School Managment/SchoolManagment.Infrustructure/obj/Debug/net8.0/SchoolManagment.Infrustructure.AssemblyInfo.cs
+++ School Managment/SchoolManagment.Infrustructure/obj/Debug/net8.0/SchoolManagment.Infrustructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagment.Infrustructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771ce56e000476186f4ef4c7b2c7eda806a33622")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagment.Infrustructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagment.Infrustructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- School Managment/SchoolManagment.Service/obj/Debug/net8.0/SchoolManagment.Service.AssemblyInfo.cs
+++ School Managment/SchoolManagment.Service/obj/Debug/net8.0/SchoolManagment.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagment.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771ce56e000476186f4ef4c7b2c7eda806a33622")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagment.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagment.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
