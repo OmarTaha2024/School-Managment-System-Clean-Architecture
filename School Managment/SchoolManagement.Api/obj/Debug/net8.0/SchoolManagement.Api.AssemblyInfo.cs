853cf4cf70b394cced4d9ca2d53dc7ac99619ea1
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-05-18 17:50:54 +0300

    merge success

--- School Managment/SchoolManagement.Api/obj/Debug/net8.0/SchoolManagement.Api.AssemblyInfo.cs
+++ School Managment/SchoolManagement.Api/obj/Debug/net8.0/SchoolManagement.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+479de7e201fd55980959c0eeb6d549a41d4652eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
