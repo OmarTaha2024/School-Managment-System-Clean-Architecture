769f55b853c03e7159913c876e957858c1ce9ae9
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-05-19 22:47:20 +0300

    last changes

--- School Managment/SchoolManagement.Api/obj/Debug/net8.0/SchoolManagement.Api.AssemblyInfo.cs
+++ School Managment/SchoolManagement.Api/obj/Debug/net8.0/SchoolManagement.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16963ef1b649688b812e3ab58eb3c0377d110318")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- School Managment/SchoolManagment.Data/obj/Debug/net8.0/SchoolManagment.Data.AssemblyInfo.cs
+++ School Managment/SchoolManagment.Data/obj/Debug/net8.0/SchoolManagment.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagment.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16963ef1b649688b812e3ab58eb3c0377d110318")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagment.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagment.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
