1c6a36918b79714dc4a536aa66cc79278101adcd
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-05-21 15:42:37 +0300

    solve conflicts

--- School Managment/SchoolManagment.Infrustructure/obj/Debug/net8.0/SchoolManagment.Infrustructure.AssemblyInfo.cs
+++ School Managment/SchoolManagment.Infrustructure/obj/Debug/net8.0/SchoolManagment.Infrustructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagment.Infrustructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48a6abaf7b973e73fa056859647c857046d45be1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagment.Infrustructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagment.Infrustructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
